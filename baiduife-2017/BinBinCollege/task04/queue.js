27ce37b4e0fb7a70a27268818cb67430d6d6390f
aicanxxx <985568626@qq.com>
2017-03-21 23:08:43 +0800

    task04

---
+++ baiduife-2017/BinBinCollege/task04/queue.js
@@ -0,0 +1,99 @@
+/**
+ * Created by Administrator on 2017/3/21 0021.
+ */
+
+function addLoadEvent(func){
+    var oldonload=window.onload;
+    if(typeof window.onload!='function'){
+        window.onload=func;
+    }else {
+        window.onload=function(){
+            oldonload();
+            func();
+        }
+    }
+}
+function addClick(eleId,func){
+    if(!document.getElementById)return false;
+    var id=document.getElementById(eleId);
+    id.onclick=func;
+
+}
+function checkNumber(theObj) {
+    var reg = /^[0-9]+.?[0-9]*$/;
+    if(reg.test(theObj)) {
+        return true;
+    }
+    return false;
+}
+
+function newLiNode(){
+    var input=document.getElementById('input');
+    var list=document.createElement('li');
+    if(!checkNumber(input.value)){
+        alert('输入有问题，请正确输入数字');
+        return false;
+    }else{
+        list.innerHTML=input.value;
+        list.setAttribute('class','queueEle');
+        return list;
+    }
+}
+function leftIn(){
+    var queue=document.getElementById('queue');
+    var eleNode=newLiNode();
+    queue.insertBefore(eleNode,queue.firstChild);
+}
+
+function rightIn(){
+    var queue=document.getElementById('queue');
+    var eleNode=newLiNode();
+    queue.appendChild(eleNode);
+}
+function leftOut(){
+    var queue=document.getElementById('queue');
+    if(queue.childElementCount!=0){
+        alert(queue.firstChild.innerHTML);
+        queue.removeChild(queue.firstChild);
+    } else {
+        alert('队列中没有元素')
+    }
+
+}
+function rightOut(){
+    var queue=document.getElementById('queue');
+    if(queue.childElementCount!=0){
+        alert(queue.lastChild.innerHTML);
+        queue.removeChild(queue.lastChild);
+    } else {
+        alert('队列中没有元素')
+    }
+}
+function btnAddClick(){
+    addClick('btn1',leftIn);
+    addClick('btn2',rightIn);
+    addClick('btn3',leftOut);
+    addClick('btn4',rightOut);
+}
+function liAddClick(){
+    var li=document.getElementsByClassName('queueEle');
+    li.onclick=function(){
+        var data=this.innerHTML;
+        this.parentNode.removeChild(this);
+        alert(data);
+    }
+}
+//btnAddClick();
+addLoadEvent(btnAddClick);
+addLoadEvent(liAddClick);
+
+
+
+/*function insertAfter(newNode,targetNode){
+ var parent=targetNode.parentNode;
+ if(parent.lastChild==targetNode){
+ parent.appendChild(newNode);
+ }else{
+ parent.insertBefore(newNode,targetNode.nextSibling);
+ }
+ }*/
